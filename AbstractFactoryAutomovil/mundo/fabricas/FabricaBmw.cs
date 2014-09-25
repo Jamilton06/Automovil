1123208508b28e96a0df08f394181acedad9aa01
jamilton06 <jamiltonmeneses06@gmail.com>
2014-09-25 18:22:08 -0500

    automivil Bmw

---
+++ AbstractFactoryAutomovil/mundo/fabricas/FabricaBmw.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AbstractFactoryAutomovil.mundo.interfaces;
+using AbstractFactoryAutomovil.mundo.clases;
+
+namespace AbstractFactoryAutomovil.mundo.fabricas
+{
+    class FabricaBmw: IFabricaPartes
+    {
+        public IAuto crearAuto()
+        {
+            return new AutoBmw();
+        }
+
+        public IMotor crearMotor()
+        {
+            return new MotorBmw();
+        }
+
+        public ICarroceria crearCarroceria()
+        {
+            return new CarroceriaBmw();
+        }
+    }
+}
